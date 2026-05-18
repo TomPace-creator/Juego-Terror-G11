c46cce2e393a5a9a530844fcff75e10733b4f1b5
M0R8 <eze.mor8@gmail.com>
2026-05-18 18:19:41 -0300

    arreglo enemigo puertas cerradas con llave
    el enemy me abria la puerta del atico! ya no

--- Assets/_OtherAssets (Ez)/_Scripts/Player/PlayerSanity.cs
+++ Assets/_OtherAssets (Ez)/_Scripts/Player/PlayerSanity.cs
@@ -28,8 +28,8 @@ public class PlayerSanity : MonoBehaviour
 
     private void Start()
     {
-        //cordura al 70
-        currentSanity = maxSanity * 0.7f;
+        //cordura al 90
+        currentSanity = maxSanity * 0.9f;
 
         if (sanityVignette != null)
         {
